94eb4d4d86f6bcdf1aca2065c99b068b2114d896
Arseny <43936298+RainCatalyst@users.noreply.github.com>
2023-08-09 17:27:41 +0200

    Switched to URP and added particles

--- Assets/PlayerAnimations.cs
+++ Assets/PlayerAnimations.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PlayerAnimations : MonoBehaviour
 {
@@ -10,12 +11,15 @@ public class PlayerAnimations : MonoBehaviour
     private Transform head;
     [SerializeField]
     private Transform visualBody;
+    [SerializeField]
+    private VisualEffect snapEffect;
 
     public void Socket()
     {
         LeanTween.moveLocalY(bodyTop.gameObject, 0.34f, 0.25f).setEaseInBack();
         LeanTween.moveLocalY(head.gameObject, 0.96f, 0.25f).setEaseInBack();
         LeanTween.moveLocalY(visualBody.gameObject, -2.4f, 0.35f).setEaseInBack();
+        LeanTween.delayedCall(gameObject, 0.1f, PlaySnapEffect);
     }
 
     public void Unsocket()
@@ -23,5 +27,11 @@ public class PlayerAnimations : MonoBehaviour
         LeanTween.moveLocalY(bodyTop.gameObject, 0.62f, 0.15f).setEaseOutCubic();
         LeanTween.moveLocalY(head.gameObject, 1.12f, 0.15f).setEaseOutCubic();
         LeanTween.moveLocalY(visualBody.gameObject, -1.24f, 0.20f).setEaseOutCubic();
+    }
+
+    private void PlaySnapEffect()
+    {
+        snapEffect.Reinit();
+        snapEffect.Play();
     }
 }
